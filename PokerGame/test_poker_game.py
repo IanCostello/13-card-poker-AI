9cd6dd676f506560939f8c6506063fb151d05e2b
IanCostello <iancostello1@gmail.com>
2019-09-21 11:09:50 -0400

    Implemented hand-ranking system
    Preliminary scoring system working

--- PokerGame/test_poker_game.py
+++ PokerGame/test_poker_game.py
@@ -1,10 +1,41 @@
-from HandScorer import HandScorer
-from PokerGame import Card
+from poker_game import HandScorer, Card, Rankings
+
+CLUBS = 1
+SPADES = 2
 
 def test_basic():
     assert 1 == 1
 
 def test_double():
-    Deck = [Card(Card.Suits.Clubs, 2), Card(Card.Suits.Spades, 2)]
-    scorer = HandScorer(Deck)
-    assert scorer.hasPair()
+    deck = [Card(CLUBS, 2), Card(SPADES, 2)]
+    ranking = HandScorer.build_power_range(deck)
+    assert ranking[0][0] == Rankings.PAIR.value
+
+def test_three_of_kind():
+    deck = [Card(CLUBS, 2), Card(SPADES, 2), Card(SPADES, 2)]
+    ranking = HandScorer.build_power_range(deck)
+    assert ranking[0][0] == Rankings.THREE_CARD.value
+
+def test_four_of_kind():
+    deck = [Card(CLUBS, 2), Card(SPADES, 2), Card(SPADES, 2), Card(SPADES, 2)]
+    ranking = HandScorer.build_power_range(deck)
+    assert ranking[0][0] == Rankings.FOUR_CARD.value
+
+def test_straight():
+    deck = [Card(SPADES, 2), Card(CLUBS, 3), Card(CLUBS, 4),Card(CLUBS, 5), Card(CLUBS, 6)]
+    ranking = HandScorer.build_power_range(deck)
+    assert ranking[0][0] == Rankings.STRAIGHT.value
+
+def test_flush():
+    deck = [Card(CLUBS, 2), Card(CLUBS, 3), Card(CLUBS, 4),Card(CLUBS, 5), Card(CLUBS, 8)]
+    ranking = HandScorer.build_power_range(deck)
+    assert ranking[0][0] == Rankings.FLUSH.value
+
+
+def test_straight_flush():
+    deck = [Card(CLUBS, 2), Card(CLUBS, 3), Card(CLUBS, 4),Card(CLUBS, 5), Card(CLUBS, 6)]
+    ranking = HandScorer.build_power_range(deck)
+    assert ranking[0][0] == Rankings.STRAIGHT_FLUSH.value
+
+
+
