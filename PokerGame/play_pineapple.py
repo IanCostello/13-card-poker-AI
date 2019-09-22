1360503dfc95d84f8806a47bd54bb91716e1d405
IanCostello <iancostello1@gmail.com>
2019-09-21 23:30:10 -0400

    Fixed flush

--- PokerGame/play_pineapple.py
+++ PokerGame/play_pineapple.py
@@ -13,16 +13,17 @@ def getDeal(game):
     print_game_state(game, hand)
     print("Which card do you want to play first?")
     print(f"Cards To Play -- {hand}")
+    print(f"Score {game.score_board()}")
     first_card_num = int(input("Card To Play (1,2,3): "))
     first_card_location = int(input("Row To Play (Bottom = 1, Middle = 2, Top = 3): "))
     first_card = hand.pop(first_card_num - 1)
 
     print("Which card do you want to play second?")
     print(f"Cards To Play -- {hand}")
+    print(f"Score {game.score_board()}")
     second_card_num = int(input("Card To Play (1,2): "))
     second_card_location = int(input("Row To Play (Bottom = 1, Middle = 2, Top = 3): "))
     second_card = hand.pop(second_card_num - 1)
-
     game.playIntoHand(first_card, first_card_location - 1, second_card, second_card_location - 1)
 
 
